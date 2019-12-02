cacf178c333628238cf3b5c6515cf5de79a1d954
Oded Shoshan <oded@monkeytech.co.il>
2019-12-02 14:06:12 +0200

    sd

--- app/api/helpers/usage_helpers.rb
+++ app/api/helpers/usage_helpers.rb
@@ -7,7 +7,7 @@ module UsageHelpers
       params :usage_attributes do
         requires  :product_id, type: Integer, allow_blank: false, desc: "The usage product id"
 
-        end      
+      end      
       params :usage_symptom_influance_attributes do 
 				optional :user_symptom_id, type: Integer, allow_blank: false, desc: "A valid symptom id"
 				optional :influance, type: Integer, allow_blank: false, desc: "influance value"
@@ -17,6 +17,6 @@ module UsageHelpers
 				@usage = @current_user.usages.find_by(id: params[:usage_id])
 				render_error(API::RESPONSE_CODES[:bad_request], 'Invalid usage id') unless @usage.present?
 			end
-
+    end
   end
 end
