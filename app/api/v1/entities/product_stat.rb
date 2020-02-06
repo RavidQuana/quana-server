6039e312bc7007ba25c50270805997532befb2ee
Oded Shoshan <oded@monkeytech.co.il>
2020-02-06 15:42:56 +0200

    asd

--- app/api/v1/entities/product_stat.rb
+++ app/api/v1/entities/product_stat.rb
@@ -5,10 +5,18 @@ module V1
 				expose :product, with: 'V1::Entities::Enums::Product', documentation: { 
 					type: 'Product'}
 				expose :usage_count, documentation: { type: 'Integer' }
-				expose :symptoms, with: 'V1::Entities::UsageSymptomInfluences::Base', documentation: { 
-					type: 'UsageSymptomInfluence', is_array: true }	
+				expose :symptoms, with: 'V1::Entities::ProductStatSymtpomAverage::Base', documentation: { 
+					type: 'ProductStatSymtpomAverage', is_array: true }	
 				expose :side_effects, with: 'V1::Entities::Enums::SideEffect', documentation: { 
 					type: 'SideEffect', is_array: true }		
+			end
+		end
+
+		module ProductStatSymtpomAverage
+			class Base < Grape::Entity
+				expose :symptom, with: 'V1::Entities::Enums::Symptom', documentation: { 
+					type: 'Symptom'}
+				expose :average, documentation: { type: 'Integer' }
 			end
 		end
 	end
